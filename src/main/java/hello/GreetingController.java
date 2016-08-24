d1941e77d20bb03ba251cd159d5dcbe618c592b0
Christian Nuss <christian.nuss@gmail.com>
2016-08-24 12:26:00 -0700

    Add hostname to Greeting

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -8,12 +8,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
+	static { 
+
+	}
+
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+                            String.format(template, name),
+                            getHostName());
+    }
+
+    private String getHostName() {
+    	InetAddress ip;
+        String hostname;
+        try {
+            ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+        } catch (UnknownHostException e) {
+            return "Unknown: " + e.getMessage()
+        }
+
+        return hostname;
     }
 }
